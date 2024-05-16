a785b50f6112b9ae271389d0075e81fd30257283
Tianpu Zhao <pacosynthesis@gmail.com>
2024-05-16 15:13:02 -0500

    add license, modified figure settings

--- ztpcraft/util/figure_settings.py
+++ ztpcraft/util/figure_settings.py
@@ -41,3 +41,45 @@ MPL_PRESET["PhysRevTwoCol"] = {
     "font.family": "times",
     "figure.dpi": 300,
 }
+
+# COLOR ---------------------#
+COLOR_PALETTE = {}
+COLOR_PALETTE["RdYlBu_5"] = ["#d7191c", "#fdae61", "#ffffbf", "#abd9e9", "#2c7bb6"]
+COLOR_PALETTE["RdYlBu_4"] = ["#d7191c", "#fdae61", "#abd9e9", "#2c7bb6"]
+COLOR_PALETTE["RdYlBu_11"] = [
+    "#a50026",
+    "#d73027",
+    "#f46d43",
+    "#fdae61",
+    "#fee090",
+    "#ffffbf",
+    "#e0f3f8",
+    "#abd9e9",
+    "#74add1",
+    "#4575b4",
+    "#313695",
+]
+COLOR_PALETTE["RdBu_9"] = [
+    "#b2182b",
+    "#d6604d",
+    "#f4a582",
+    "#fddbc7",
+    "#f7f7f7",
+    "#d1e5f0",
+    "#92c5de",
+    "#4393c3",
+    "#2166ac",
+]
+COLOR_PALETTE["RdBu_11"] = [
+    "#67001f",
+    "#b2182b",
+    "#d6604d",
+    "#f4a582",
+    "#fddbc7",
+    "#f7f7f7",
+    "#d1e5f0",
+    "#92c5de",
+    "#4393c3",
+    "#2166ac",
+    "#053061",
+]
